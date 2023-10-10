d3a7a7b2823a4a79b71fe715a0f05a94533a1ca6
evechen1226 <eveliving2021@gmail.com>
2023-10-10 14:24:16 +0800

    for 迴圈練習1

--- loop.php
+++ loop.php
@@ -1,2 +1,21 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+$sum=0;
+for($i=1;$i<=100;$i=$i+1){
+  // $sum=$sum+$i;        →→→詳細寫法
+  $sum +=$i;
+  echo "<br>";
+  echo $sum;
+}
+  echo "<br>";
+  echo "1加到100為" .$sum;
 ?>
+</body>
+</html>
