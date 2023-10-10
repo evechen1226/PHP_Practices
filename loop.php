c3106a4521f34df923322ea0c76d09d87ebb02b8
evechen1226 <eveliving2021@gmail.com>
2023-10-10 15:01:58 +0800

    echo 內 () 的使用方法

--- loop.php
+++ loop.php
@@ -1,21 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
+
   <?php
 $sum=0;
 for($i=1;$i<=100;$i=$i+1){
-  // $sum=$sum+$i;        →→→詳細寫法
+ 
+ //使用雙引號 "  " 時，中間的 $i 會被當成變數，而不是字串
+ //echo "當$i=" . $i ."時" ; 
+  echo '當$i=' . $i .'時' ; 
+  
+//用 ( 算式 ) 可直接顯示算式結果，但不會賦值
+  echo '$sum+$i的結果是' .($sum + $i) ;
+  echo '<br>';
+
+  echo ' ($sum + $i + 2 ) ' .($sum + $i + 2) ;
+  echo '<br>';
+
+  echo $sum . '←只有 $sum ';
+  echo '<br>';
+
+// $sum=$sum+$i;        →→→詳細寫法
   $sum +=$i;
-  echo "<br>";
-  echo $sum;
+
+
+
 }
-  echo "<br>";
-  echo "1加到100為" .$sum;
+  echo '<br>';
+  echo '1加到100為' .$sum;
 ?>
-</body>
-</html>
