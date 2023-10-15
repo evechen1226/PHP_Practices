7996095878a2cffa46e1730310ac751c59a20a5b
evechen1226 <eveliving2021@gmail.com>
2023-10-15 18:22:07 +0800

    排版

--- while_number.php
+++ while_number.php
@@ -1,61 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-<style>
-table, td, th {
-  border: 1px solid;
-}
+  <style>
+    table,
+    td,
+    th {
+      border: 1px solid;
+    }
 
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
+    table {
+      width: 100%;
+      border-collapse: collapse;
+    }
   </style>
 
 </head>
+
 <body>
-<table>
-  <tr>
-    <td><?php
+  <table>
+    <tr>
+      <td><?php
 
-$i = 1;
+          $i = 1;
 
-while($i<=100){
+          while ($i <= 100) {
 
-echo $i . '<br>';
+            echo $i . '<br>';
 
-// $i = 1+2
-$i = $i+2 ;
-
-};
-
-
-  ?></td>
-    <td>
-      <?php
-
-$sum = 0;
-$i = 1;
-
-while($i<=100){
-
-  $sum = $sum + 10;
-  $i ++ ;
-  echo $sum . '<br>';
-  if ($sum >= 100) {
-        break; 
-    };
-
-};
+            // $i = 1+2
+            $i = $i + 2;
+          };
 
 
-  ?></td>
-    <td></td>
-  </tr>
-</table>
+          ?></td>
+      <td>
+        <?php
+
+        $sum = 0;
+        $i = 1;
+
+        while ($i <= 100) {
+
+          $sum = $sum + 10;
+          $i++;
+          echo $sum . '<br>';
+          if ($sum >= 100) {
+            break;
+          };
+        };
+
+
+        ?></td>
+      <td></td>
+    </tr>
+  </table>
 
 </body>
+
 </html>
