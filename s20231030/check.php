cc820ff03530e72fc59f765ec4695cb4c7863081
evechen1226 <eveliving2021@gmail.com>
2023-10-30 13:10:22 +0800

    增加登出功能

--- s20231030/check.php
+++ s20231030/check.php
@@ -11,7 +11,7 @@ if($_POST['acc']=='admin' && $_POST['pw']=='1234'){
     header("location:./member.php");
 }else{
  
-    //header("location:login.php?m=帳號或密碼錯誤,請重新登入");
+    //header("location:login.php?m=帳號或密碼錯誤,請重新登入");+9
 
     $_SESSION['error']="帳號或密碼錯誤,請重新登入";
     header("location:./login.php");
